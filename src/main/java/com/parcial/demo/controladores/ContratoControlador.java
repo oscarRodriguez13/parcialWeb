55830d6740deea94494d3a151e72d0f8c92bc695
Oscar Rodriguez <rodriguez.oalejandro@javeriana.edu.cogit config --global user.name Oscar>
2024-11-19 17:50:42 -0500

    Commit final

--- src/main/java/com/parcial/demo/controladores/ContratoControlador.java
+++ src/main/java/com/parcial/demo/controladores/ContratoControlador.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contratos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ContratoControlador {
 
     @Autowired
